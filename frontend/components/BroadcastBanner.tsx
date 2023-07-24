adb4e30d662ba502cd86bb5b05d976314ed5bb03
Arnaud Brousseau <rno@turnkey.io>
2023-07-24 10:57:27 -0500

    Stop disabling inputs

--- frontend/components/BroadcastBanner.tsx
+++ frontend/components/BroadcastBanner.tsx
@@ -18,7 +18,7 @@ export function BroadcastBanner(props: BannerProps) {
         return (
         <div className="relative p-4 m-8 text-sm text-green-800 rounded-md bg-green-200" role="alert">
             <span onClick={onClose} className="absolute block top-2 right-4 font-bold cursor-pointer">✕</span>
-            <span className="font-medium">Successfully broadcast!</span><br />
+            <span className="font-medium">Successfully broadcast! Transaction hash: <pre>{props.txHash}</pre></span><br />
             <Link target="_blank" href={"https://sepolia.etherscan.io/tx/" + props.txHash } className="font-bold underline">View on Etherscan</Link>
         </div>
         )
