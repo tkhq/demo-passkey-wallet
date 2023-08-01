360dd8c8a7fefd37fa0b3ca6aef0eba25f9db1ba
Arnaud Brousseau <rno@turnkey.io>
2023-08-01 13:45:18 -0500

    Add space in footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
         <footer className="m-8 grid-cols-3 text-center text-zinc-400 text-xs">
           <p>
             © 2023 Turnkey Global, Inc. All rights reserved.
-    
+            {' '}
             <Link className="underline" target="_blank" href="https://github.com/tkhq/demo-passkey-wallet#legal-disclaimer">Legal disclaimer</Link>.
           </p>
         </footer>
